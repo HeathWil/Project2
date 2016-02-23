0bd4603f21ca194f4fe47d84739daf9dff90f510
HeathWil <Heather.o.wilson@ou.edu>
2016-02-23 08:59:42 -0600

    Create SeasonComparator.java

---
+++ SeasonComparator.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+/**
+ * 
+ * ***********Feb 19, 2016**********
+ *
+ *
+ *This class is going to be used if the user decides to search by seasonNumber, 
+ *and will search as such.
+ */
+public class SeasonComparator {
+	
+	/**
+	 * This method is where the comparing by season Number will be done
+	 * @param seasonNum, A String representation of the season 
+	 * @return int, which will determine whether the season is present or not.
+	 */
+	public static int compareTo(String seasonNum, ArrayList<TV> tv){
+		int i=0;
+		
+		
+		return i;
+	}
+
+}
