909740eaaebfadac9f77b1c8bc59d82a061672b1
HeathWil <Heather.o.wilson@ou.edu>
2016-03-01 20:38:06 -0600

    Update TVTest.java

--- TVTest.java
+++ TVTest.java
@@ -14,27 +14,36 @@ public class TVTest {
 
 	@Test
 	public void test() {
-		
-		
-		String tv="Something";
-		
+
+
+		String tv="\"Star Trek: The Continuing Mission\" (2007) {We Will Control All That You See and Hear (#1.6)} {{SUSPENDED}}	2010";
+
 		TV tvTest= new TV(tv);
-		
-		
-		
-		
-		Assert.assertEquals(tvTest.getTitle(), actual);
-		Assert.assertEquals(tvTest.getEpisode(), actual);
-		Assert.assertEquals(tvTest.getEpisodeNum(), actual);
-		Assert.assertEquals(tvTest.getIntroYear(), actual);
-		Assert.assertEquals(tvTest.getSeasonNum(), actual);
-		Assert.assertEquals(tvTest.getYears(), actual);
-		
-		
-		
+
+		String title="\"Star Trek: The Continuing Mission\"";
+		String introYear="(2007)";
+		String episode="We Will Control All That You See and Hear";
+		String seasonNum="#1";
+		String episodeNum="6";
+		String suspended="{{SUSPENDED}}";
+		String years="2010";
+
+
+		Assert.assertEquals(tvTest.getTitle(), title);
+		Assert.assertEquals(tvTest.getEpisode(), episode);
+		Assert.assertEquals(tvTest.getEpisodeNum(), episodeNum);
+		Assert.assertEquals(tvTest.getIntroYear(), introYear);
+		Assert.assertEquals(tvTest.getSeasonNum(), seasonNum);
+		Assert.assertEquals(tvTest.getYears(), years);
+		Assert.assertEquals(tvTest.getSuspended(), suspended);
+
+
+
 		String words="happy am I";
-		
-		Assert.assertEquals(TV.reverse(words), actual);
+
+		String result="I am happy";
+
+		Assert.assertEquals(TV.reverse(words), result);
 	}
 
 }
